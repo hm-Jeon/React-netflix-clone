ebfb794bb5f9350cff168fe2a6d861cd3c16ca36
hm-Jeon <jhm971102@gmail.com>
2022-06-09 18:17:04 +0900

    Search 기능 추가
    - v1
    - query에 대한 movie, tv 검색 결과 표시

---
+++ src/Routes/Search/Search.styled.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  min-height: 100vh;
+  padding-top: 120px;
+  background-color: ${props => props.theme.black.veryDark};
+`;
--- src/Routes/Search/Search.tsx
+++ src/Routes/Search/Search.tsx
@@ -1,10 +1,56 @@
+import { useQuery } from "react-query";
 import { useLocation } from "react-router-dom";
+import {
+  IGetMoviesResult,
+  IGetTvResult,
+  searchMovie,
+  searchTv,
+} from "../../api";
+import Slider from "../../Components/Slider/Slider";
+import { Sliders } from "../../Components/Slider/Slider.styled";
+import { Loader } from "../Home/Home.styled";
+import { Wrapper } from "./Search.styled";
 
 function Search() {
   // URLSearchParams: URL string에서 parameter값을 parsing해준다. get() 함수를 이용한다.
   const keyword = new URLSearchParams(useLocation().search).get("keyword");
-  console.log(keyword);
-  return null;
+
+  const { isLoading: searchMovieLoading, data: searchMovieData } =
+    useQuery<IGetMoviesResult>(["search", "movie"], () =>
+      searchMovie(keyword!)
+    );
+
+  const { isLoading: searchTvLoading, data: searchTvData } =
+    useQuery<IGetTvResult>(["search", "tv"], () => searchTv(keyword!));
+
+  const isLoading = searchMovieLoading || searchTvLoading;
+
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading</Loader>
+      ) : (
+        <>
+          <Sliders>
+            <Slider
+              sliderName="movie"
+              movieData={searchMovieData!}
+              cutOutRemainder={false}
+              slider_col={6}
+              slice_first={false}
+            />
+            <Slider
+              sliderName="tv"
+              tvData={searchTvData!}
+              cutOutRemainder={false}
+              slider_col={6}
+              slice_first={false}
+            />
+          </Sliders>
+        </>
+      )}
+    </Wrapper>
+  );
 }
 
 export default Search;
